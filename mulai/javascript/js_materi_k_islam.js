8856ae1172141cc4dc2c92cef0881de56ca65e4b
Moh. Zainul Muttaqin | Zalepik <51511976+mzainulmuttaqin@users.noreply.github.com>
2023-04-11 14:08:53 +0700

    2

---
+++ mulai/javascript/js_materi_k_islam.js
@@ -0,0 +1,163 @@
+
+
+function btn_materi_penjelasan_1(){
+    document.getElementById("d-sound_materi_k_islam").src = "../../sound/peta/kerajaan_islam/sound_1.mp3";
+    document.getElementById("d-sound_materi_k_islam").load();
+    document.getElementById("d-sound_materi_k_islam").play();
+    document.getElementsByClassName("d-materi_penjelasan_1")[0].classList.remove("d-hide");
+    document.getElementsByClassName("d-materi_peta_1")[0].classList.add("d-hide");
+}
+
+function btn_materi_penjelasan_2(){
+    document.getElementById("d-sound_materi_k_islam").src = "../../sound/peta/kerajaan_islam/sound_2.mp3";
+    document.getElementById("d-sound_materi_k_islam").load();
+    document.getElementById("d-sound_materi_k_islam").play();
+    document.getElementsByClassName("d-materi_penjelasan_2")[0].classList.remove("d-hide");
+    document.getElementsByClassName("d-materi_peta_2")[0].classList.add("d-hide");
+}
+
+function btn_materi_penjelasan_3(){
+    document.getElementById("d-sound_materi_k_islam").src = "../../sound/peta/kerajaan_islam/sound_3.mp3";
+    document.getElementById("d-sound_materi_k_islam").load();
+    document.getElementById("d-sound_materi_k_islam").play();
+    document.getElementsByClassName("d-materi_penjelasan_3")[0].classList.remove("d-hide");
+    document.getElementsByClassName("d-materi_peta_3")[0].classList.add("d-hide");
+}
+
+function btn_materi_penjelasan_4(){
+    document.getElementById("d-sound_materi_k_islam").src = "../../sound/peta/kerajaan_islam/sound_4.mp3";
+    document.getElementById("d-sound_materi_k_islam").load();
+    document.getElementById("d-sound_materi_k_islam").play();
+    document.getElementsByClassName("d-materi_penjelasan_4")[0].classList.remove("d-hide");
+    document.getElementsByClassName("d-materi_peta_4")[0].classList.add("d-hide");
+}
+
+function btn_materi_penjelasan_5(){
+    document.getElementById("d-sound_materi_k_islam").src = "../../sound/peta/kerajaan_islam/sound_5.mp3";
+    document.getElementById("d-sound_materi_k_islam").load();
+    document.getElementById("d-sound_materi_k_islam").play();
+    document.getElementsByClassName("d-materi_penjelasan_5")[0].classList.remove("d-hide");
+    document.getElementsByClassName("d-materi_peta_5")[0].classList.add("d-hide");
+}
+
+function btn_materi_penjelasan_6(){
+    document.getElementById("d-sound_materi_k_islam").src = "../../sound/peta/kerajaan_islam/sound_6.mp3";
+    document.getElementById("d-sound_materi_k_islam").load();
+    document.getElementById("d-sound_materi_k_islam").play();
+    document.getElementsByClassName("d-materi_penjelasan_6")[0].classList.remove("d-hide");
+    document.getElementsByClassName("d-materi_peta_6")[0].classList.add("d-hide");
+}
+
+function btn_materi_peta_1(){
+
+    document.getElementsByClassName("d-materi_penjelasan_1")[0].classList.add("d-hide");
+    document.getElementsByClassName("d-materi_peta_1")[0].classList.remove("d-hide");
+}
+
+function btn_materi_peta_2(){
+
+    document.getElementsByClassName("d-materi_penjelasan_2")[0].classList.add("d-hide");
+    document.getElementsByClassName("d-materi_peta_2")[0].classList.remove("d-hide");
+}
+
+function btn_materi_peta_3(){
+
+    document.getElementsByClassName("d-materi_penjelasan_3")[0].classList.add("d-hide");
+    document.getElementsByClassName("d-materi_peta_3")[0].classList.remove("d-hide");
+}
+
+function btn_materi_peta_4(){
+
+    document.getElementsByClassName("d-materi_penjelasan_4")[0].classList.add("d-hide");
+    document.getElementsByClassName("d-materi_peta_4")[0].classList.remove("d-hide");
+}
+
+function btn_materi_peta_5(){
+
+    document.getElementsByClassName("d-materi_penjelasan_5")[0].classList.add("d-hide");
+    document.getElementsByClassName("d-materi_peta_5")[0].classList.remove("d-hide");
+}
+
+function btn_materi_peta_6(){
+
+    document.getElementsByClassName("d-materi_penjelasan_6")[0].classList.add("d-hide");
+    document.getElementsByClassName("d-materi_peta_6")[0].classList.remove("d-hide");
+}
+
+function btn_materi_lanjut_1(){
+
+    document.getElementsByClassName("d-materi_penjelasan_1")[0].classList.add("d-hide");
+    document.getElementsByClassName("d-materi_peta_2")[0].classList.remove("d-hide");
+    document.getElementById("d-sound_materi_k_islam").pause();
+}
+
+function btn_materi_lanjut_2(){
+
+    document.getElementsByClassName("d-materi_penjelasan_2")[0].classList.add("d-hide");
+    document.getElementsByClassName("d-materi_peta_3")[0].classList.remove("d-hide");
+    document.getElementById("d-sound_materi_k_islam").pause();
+}
+
+function btn_materi_lanjut_3(){
+
+    document.getElementsByClassName("d-materi_penjelasan_3")[0].classList.add("d-hide");
+    document.getElementsByClassName("d-materi_peta_4")[0].classList.remove("d-hide");
+    document.getElementById("d-sound_materi_k_islam").pause();
+}
+
+function btn_materi_lanjut_4(){
+
+    document.getElementsByClassName("d-materi_penjelasan_4")[0].classList.add("d-hide");
+    document.getElementsByClassName("d-materi_peta_5")[0].classList.remove("d-hide");
+    document.getElementById("d-sound_materi_k_islam").pause();
+}
+
+function btn_materi_lanjut_5(){
+
+    document.getElementsByClassName("d-materi_penjelasan_5")[0].classList.add("d-hide");
+    document.getElementsByClassName("d-materi_peta_6")[0].classList.remove("d-hide");
+    document.getElementById("d-sound_materi_k_islam").pause();
+}
+
+function btn_materi_lanjut_6(){
+
+    document.getElementsByClassName("d-materi_penjelasan_6")[0].classList.add("d-hide");
+    document.getElementsByClassName("d-materi_peta_7")[0].classList.remove("d-hide");
+    document.getElementById("d-sound_materi_k_islam").pause();
+}
+
+function btn_materi_peta_kembali_2(){
+    document.getElementsByClassName("d-materi_penjelasan_1")[0].classList.remove("d-hide");
+    document.getElementsByClassName("d-materi_peta_2")[0].classList.add("d-hide");
+    document.getElementById("d-sound_materi_k_islam").pause();
+}
+
+function btn_materi_peta_kembali_3(){
+    document.getElementsByClassName("d-materi_penjelasan_2")[0].classList.remove("d-hide");
+    document.getElementsByClassName("d-materi_peta_3")[0].classList.add("d-hide");
+    document.getElementById("d-sound_materi_k_islam").pause();
+}
+
+function btn_materi_peta_kembali_4(){
+    document.getElementsByClassName("d-materi_penjelasan_3")[0].classList.remove("d-hide");
+    document.getElementsByClassName("d-materi_peta_4")[0].classList.add("d-hide");
+    document.getElementById("d-sound_materi_k_islam").pause();
+}
+
+function btn_materi_peta_kembali_5(){
+    document.getElementsByClassName("d-materi_penjelasan_4")[0].classList.remove("d-hide");
+    document.getElementsByClassName("d-materi_peta_5")[0].classList.add("d-hide");
+    document.getElementById("d-sound_materi_k_islam").pause();
+}
+
+function btn_materi_peta_kembali_6(){
+    document.getElementsByClassName("d-materi_penjelasan_5")[0].classList.remove("d-hide");
+    document.getElementsByClassName("d-materi_peta_6")[0].classList.add("d-hide");
+    document.getElementById("d-sound_materi_k_islam").pause();
+}
+
+function btn_materi_peta_kembali_7(){
+    document.getElementsByClassName("d-materi_penjelasan_6")[0].classList.remove("d-hide");
+    document.getElementsByClassName("d-materi_peta_7")[0].classList.add("d-hide");
+    document.getElementById("d-sound_materi_k_islam").pause();
+}
